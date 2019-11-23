a313124c844a363d7d3e8706f41317e1390a7b43
Partenie Alex <parteniealex@yahoo.com>
2019-11-23 19:42:10 +0200

    added camera functionallity, takes picture and puts in imageview

--- Filatelie3/app/src/main/java/com/partenie/alex/filatelie/AddItemActivity.java
+++ Filatelie3/app/src/main/java/com/partenie/alex/filatelie/AddItemActivity.java
@@ -1,36 +1,52 @@
 package com.partenie.alex.filatelie;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.PersistableBundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AddItemActivity extends AppCompatActivity {
     Gson gson = new Gson();
     String[] types_fromjson;
     ProgressDialog pd;
     Spinner type_spinner;
+    private Uri file;
+    View photoLayout;
+    ImageView preview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +63,48 @@ public class AddItemActivity extends AppCompatActivity {
         if (intent.getStringExtra("key") != null) {
             this.getSupportActionBar().setTitle("Edit " + intent.getStringExtra("key"));
         }
-        type_spinner = findViewById(R.id.item_type);
 
+        type_spinner = findViewById(R.id.item_type);
+        photoLayout=findViewById(R.id.stamp_photo);
+        preview=findViewById(R.id.img);
         new JsonTask().execute("https://api.myjson.com/bins/iueei");
+
+        photoLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture(view);
+            }
+        });
+
+
+    }
+
+
+    public void takePicture(View view) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 100);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+
+            Toast.makeText(this, photo.getHeight()+"", Toast.LENGTH_SHORT).show();
+
+//            preview.setMaxWidth(200);
+            preview.setImageBitmap(photo);
+
+//            imageView.setImageBitmap(photo);
+        }
     }
 
     @Override
